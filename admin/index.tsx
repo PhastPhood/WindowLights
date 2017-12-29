94aa78be3f8556ddafef368b4117c12d1f261f21
PhastPhood <jeffrey.gao@vanderbilt.edu>
2017-12-29 00:32:42 -0800

    Stylesheets base

--- admin/index.tsx
+++ admin/index.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
+import './stylesheets/main.scss';
+
 ReactDOM.render(
   <div>Hello, world!</div>,
   document.getElementById('root')
